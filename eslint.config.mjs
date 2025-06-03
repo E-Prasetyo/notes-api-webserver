aab6e0e4b290f1a4561ed357ca7abae7681fb3f0
E-Prasetyo <tarsisiusekoprasetyo11@gmail.com>
2025-06-04 01:29:30 +0700

    Note App
    add databas with postgres

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,10 @@
 import js from "@eslint/js";
 import globals from "globals";
-import pluginReact from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
-import daStyle from 'eslint-config-dicodingacademy';
 
 
 export default defineConfig([
-  daStyle,
-  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs,jsx}"], languageOptions: { globals: globals.browser } },
-  pluginReact.configs.flat.recommended,
+  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
+  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.node } },
 ]);
